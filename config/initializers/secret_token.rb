f05283bfb08f707d6febeb241a73cdc035cb0d9c
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-21 20:12:37 -0500

    Update naming error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlogChallengeNick::Application.config.secret_key_base = '956ae5c5e98536d8cdd27c9c1db875c59bce3094eed422d327d687f53e1c538223a7f122bdf5c2883ed62ad857d0eb41951d994d98e2835000ddf840d184bc3b'

9691a0543d497e68ca6d93ff8e3fd36e66651a61
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 18:08:29 -0500

    Add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
   end
 
+  private 
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end

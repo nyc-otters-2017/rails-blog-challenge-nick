376f3b2b5dbd8778c3dad845230dfb5ad288a2bd
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 22:28:16 -0500

    Only users can add a post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RailsBlogChallengeNick::Application.routes.draw do
 
   resources :posts, only: [:index, :show]
   resources :users do
-    resources :post, only: [:new, :create]
+    resources :posts, only: [:new, :create]
   end
 
   

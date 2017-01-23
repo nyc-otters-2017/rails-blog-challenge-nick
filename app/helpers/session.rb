e717083c0aa18cdc916953d812efd430f685e593
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 20:12:36 -0500

    Add login? method

--- app/helpers/session.rb
+++ app/helpers/session.rb
@@ -6,6 +6,10 @@ def current_user
   @current_user ||= User.find_by(id: session[:user_id])
 end
 
-def require_user
-  redirect '/users/new' unless current_user
+# def require_user
+#   redirect '/users/new' unless current_user
+# end
+
+def logged_in?
+  !current_user.nil?
 end

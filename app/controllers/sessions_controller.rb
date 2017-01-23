16dd5c63add3a686a97c9cae742a378fb268ee88
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 20:02:34 -0500

    Update user actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController <ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      render 
+      log_in user
+      redirect_to user
     else
       @errors = @user.errors.full_messages
       render "new"

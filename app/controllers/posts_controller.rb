7c31453e3e9b0e7ceaed32382c0b3a9665e30730
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 23:01:26 -0500

    Can post comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    if current_user.posts.create(post_params) 
+      redirect_to root_url 
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
   end
-  
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
 end

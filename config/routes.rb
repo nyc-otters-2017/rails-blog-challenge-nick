f6aa71226146d2b7b3499fdadcc17cd438bbca0f
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-01-22 19:49:00 -0500

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 RailsBlogChallengeNick::Application.routes.draw do
   root 'users#index'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 
 end
